779a79ca05bf520a9038a33598b4cb75ccaeec45
kumraushan <kumarraushanrk5@gmail.com>
2021-02-14 10:17:09 +0530

    updating portofolio section

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,22 +4,10 @@ class Portfolio extends Component {
   render() {
 
     if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
+      var projects = this.props.data.projects.map(function(Projects){
         var projectImage = 'images/portfolio/'+projects.img;
         return( 
         <div key={projects.title} className="padding-column rounded col-lg-4 col-md-6 columns portfolio-item">
-           {/* <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.desc}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div> */}
 
           <div className="frontside">
               <div className="card">
